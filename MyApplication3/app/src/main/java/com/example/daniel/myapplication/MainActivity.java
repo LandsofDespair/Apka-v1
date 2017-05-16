e6a1a26dbb1016ba5fb326a771df1166c4eda87f
near4422 <racuch4422@gmail.com>
2017-05-16 18:16:01 +0200

    Pierwszy build v0.00002
    Poprawione EditTexty, wprowadzony counterValue i prompt

--- MyApplication3/app/src/main/java/com/example/daniel/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/daniel/myapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.daniel.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counterValue = 0;
+    private TextView counterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterTextView = (TextView)findViewById(R.id.counterTextView);
+    }
+
+    public void openDialog() {
+        final Dialog dialog = new Dialog(context); // Context, this, etc.
+        dialog.setContentView(R.layout.dialog_demo);
+        dialog.setTitle(R.string.dialog_title);
+        dialog.show();
+    }
+
+    public void PlusButtonClicked(View view) {
+        if(counterValue==5)
+            openDialog()
+        else
+        counterValue++;
+        counterTextView.setText(String.valueOf(counterValue));
+    }
+
+    public void MinusButtonClicked(View view) {
+        counterValue--;
+        counterTextView.setText(String.valueOf(counterValue));
     }
 }
